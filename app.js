cba29062a3fb78e8b60c2a15eafd69e45f341b9f
Mohammad Zeeshan <zeeshanm1997@gmail.com>
2020-07-31 22:47:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -56,6 +56,6 @@ app.use("/", indexRoutes); //prefix- for shorter route declaration
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT || 3001, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("The YelpCamp Server Has Started")
 });
